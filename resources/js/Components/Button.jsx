71a4254d477b614988f5ea36697c519dee96a103
Arfal-PNM <m.arfalpermana@ptpnm.com>
2024-11-13 13:08:23 +0700

    Feature: slicing sign in & sign up

---
+++ resources/js/Components/Button.jsx
@@ -0,0 +1,19 @@
+export default function Button({
+    type = "submit",
+    className = "",
+    variant = "primary",
+    processing,
+    children,
+}) {
+    return (
+        <button
+            type={type}
+            className={`rounded-2xl py-[13px] text-center w-full btn-${variant} ${
+                processing && "opacity-30"
+            }  ${className}`}
+            disabled={processing}
+        >
+            {children}
+        </button>
+    );
+}
---
+++ resources/js/Pages/Prototype/Register.jsx
@@ -0,0 +1,102 @@
+import InputLabel from "@/Components/InputLabel";
+import TextInput from "@/Components/TextInput";
+import Button from "@/Components/Button";
+import { Link, Head } from "@inertiajs/react";
+
+export default function Register() {
+    return (
+        <>
+            <Head title="Sign Up" />
+            <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
+                <div className="fixed top-[-50px] hidden lg:block">
+                    <img
+                        src="assets/images/signup-image.png"
+                        className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]"
+                        alt=""
+                    />
+                </div>
+                <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
+                    <div>
+                        <img src="" alt="" />
+                        <div className="my-[70px]">
+                            <div className="font-semibold text-[26px] mb-3">
+                                Sign Up
+                            </div>
+                            <p className="text-base text-[#767676] leading-7">
+                                Explore our new movies and get <br />
+                                the better insight for your life
+                            </p>
+                        </div>
+                        <form className="w-[370px]">
+                            <div className="flex flex-col gap-6">
+                                <div>
+                                    <InputLabel
+                                        htmlFor="fullname"
+                                        value="Full Name"
+                                    />
+                                    <TextInput
+                                        type="text"
+                                        name="fullname"
+                                        placeholder="Your fullname..."
+                                        autoComplete="off"
+                                    />
+                                </div>
+                                <div>
+                                    <InputLabel
+                                        htmlFor="email"
+                                        value="Email Address"
+                                    />
+                                    <TextInput
+                                        type="email"
+                                        name="email"
+                                        placeholder="Your email..."
+                                        autoComplete="off"
+                                    />
+                                </div>
+                                <div>
+                                    <InputLabel
+                                        htmlFor="password"
+                                        value="Password"
+                                    />
+                                    <TextInput
+                                        type="password"
+                                        name="password"
+                                        placeholder="********"
+                                        autoComplete="off"
+                                    />
+                                </div>
+                            </div>
+                            <div className="grid space-y-[14px] mt-[30px]">
+                                <Link href={route("prototype.dashboard")}>
+                                    <Button type="submit">
+                                        <span className="text-base font-semibold">
+                                            {" "}
+                                            Sign Up{" "}
+                                        </span>
+                                    </Button>
+                                </Link>
+
+                                <Link href={route("prototype.login")}>
+                                    <Button
+                                        type="button"
+                                        variant="white-outline"
+                                    >
+                                        <span className="text-base font-semibold">
+                                            {" "}
+                                            Already have account{" "}
+                                        </span>
+                                    </Button>
+                                </Link>
+                                {/* <!-- <button type="submit" className="rounded-2xl bg-alerange py-[13px] text-center">
+                            <span className="text-base font-semibold">
+                                Sign Up
+                            </span>
+                        </button> --> */}
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
